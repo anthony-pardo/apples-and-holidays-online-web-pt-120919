9d8a4585ec84ad7a115ae9c0e12fefcc1aed6f7f
Anthony Pardo <pardo.anthony@gmail.com>
2019-12-19 21:05:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays_hash|
     puts season.capitalize 
     holidays_hash.each do |holiday, supplies|
-      puts "  #{holiday.to_s.split("_").collect {|word| word.capitalize}.join}:"
+      puts "  #{holiday.to_s.split("_").collect {|word| word.capitalize}.join(" ")}:"
     end
   end
 end
